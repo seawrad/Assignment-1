cff1319d5ba9af9ea1bbac7a9ddbb0c48d62dadd
Cheng Tsz Wang <22239480@life.hkbu.edu.hk>
2025-09-29 15:37:27 +0800

    Initial Commit

---
+++ Assignment 1/Assignment_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_1App.swift
+//  Assignment 1
+//
+//  Created by f2239480 on 29/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment 1Tests/Assignment_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_1Tests.swift
+//  Assignment 1Tests
+//
+//  Created by f2239480 on 29/9/2025.
+//
+
+import Testing
+@testable import Assignment_1
+
+struct Assignment_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
